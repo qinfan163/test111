56db4d1044e6295c49e69b17b270aa5458d11780
qinfan <qinf@youyuan.com>
2020-12-24 15:27:34 +0800

    master修改

--- src/Mandome1.java
+++ src/Mandome1.java
@@ -5,5 +5,6 @@ public class Mandome1 {
         System.out.println("Hello World");
         System.out.println("是是是");
         System.out.println("这是新建的分支");
+        System.out.println("6666666");
     }
 }
