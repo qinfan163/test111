b42296822fc6e5e5399ad6437dd092aeaa3453ea
qinfan <qinf@youyuan.com>
2020-12-24 15:19:32 +0800

    将test2分支代码合并至master分支

--- src/Mandome1.java
+++ src/Mandome1.java
@@ -3,6 +3,7 @@
 public class Mandome1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("是是是");
         System.out.println("这是新建的分支");
     }
 }
