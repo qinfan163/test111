adc274d7d79afc16245c0334fcbdf16320a2c51a
qinfan <15303754421@163.com>
2021-03-26 11:39:00 +0800

    修改分支2

--- src/Mandome1.java
+++ src/Mandome1.java
@@ -3,10 +3,6 @@
 public class Mandome1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("是是是");
-        System.out.println("这是新建的分支");
-        System.out.println("6666666");
-        System.out.println("7777");
-        System.out.println("88888");
+        System.out.println("我是分支2");
     }
 }
