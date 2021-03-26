8af40968b655bbe87b611741188bfe58dea1123e
qinfan <15303754421@163.com>
2021-03-26 13:48:31 +0800

    新增代码

--- src/Mandome1.java
+++ src/Mandome1.java
@@ -3,7 +3,6 @@
 public class Mandome1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("我是分支3");
-        System.out.println("666");
+        System.out.println("我是分支2");
     }
 }
