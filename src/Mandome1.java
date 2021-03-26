c1aef02eb7930618d651c52ba56e9541db310f46
qinfan <15303754421@163.com>
2021-03-26 15:31:08 +0800

    for循环

--- src/Mandome1.java
+++ src/Mandome1.java
@@ -1,16 +1,27 @@
 
 
 public class Mandome1 {
+    static String name = "消失";
+    static int age = 19;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("我是分支2");
-        System.out.println("我是分支3");
-        System.out.println("666");
-        Mandome1.test();
+
+        for (int i = 0; i <10 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+"*"+i+"="+i*j+"\t");
+            }
+            System.out.println(i);
+        }
+//        System.out.println("Hello World");
+//        System.out.println("我是分支2");
+//        System.out.println("我是分支3");
+//        System.out.println("666");
+//        Mandome1.test();
+
     }
 
 
-    public static void test(){
-        System.out.println(777777777);
+    public static void test() {
+        System.out.println("" + name + "..." + age);
     }
 }
